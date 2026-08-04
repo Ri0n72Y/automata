51cd3d36264ccc093feac722e3b2e7568c0fcfc0
Ri0n72Y <tzy673570751@gmail.com>
2026-08-04 14:54:57 +0800

    [codex]fix(objects): 让无限方块堆实现来源接口

--- scripts/objects/infinite_block_pile.gd
+++ scripts/objects/infinite_block_pile.gd
@@ -1,15 +1,27 @@
-extends RefCounted
+extends ItemSourceInterface
 class_name InfiniteBlockPile
 
 signal produced_count_changed(previous_count: int, current_count: int)
 
-var produced_count: int = 0
+var _produced_count: int = 0
+
+
+func get_output_item_type() -> StringName:
+	return StandardBlock.TYPE_ID
+
+
+func is_infinite() -> bool:
+	return true
+
+
+func get_produced_count() -> int:
+	return _produced_count
 
 
 func take_item() -> ItemTransferResult:
-	var previous_count := produced_count
-	produced_count += 1
-	produced_count_changed.emit(previous_count, produced_count)
+	var previous_count := _produced_count
+	_produced_count += 1
+	produced_count_changed.emit(previous_count, _produced_count)
 	return ItemTransferResult.accepted(StandardBlock.create())
 
 
@@ -18,8 +30,8 @@ func put_item(_item: Variant) -> ItemTransferResult:
 
 
 func reset() -> void:
-	if produced_count == 0:
+	if _produced_count == 0:
 		return
-	var previous_count := produced_count
-	produced_count = 0
-	produced_count_changed.emit(previous_count, produced_count)
+	var previous_count := _produced_count
+	_produced_count = 0
+	produced_count_changed.emit(previous_count, _produced_count)
