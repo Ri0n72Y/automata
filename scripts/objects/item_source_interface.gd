4c8909c178722e78c7e22d092a114a11fa63fdcd
Ri0n72Y <tzy673570751@gmail.com>
2026-08-04 14:54:12 +0800

    [codex]fix(objects): 添加统一物品来源接口

---
+++ scripts/objects/item_source_interface.gd
@@ -0,0 +1,31 @@
+extends RefCounted
+class_name ItemSourceInterface
+
+var _interaction_cells: Array[Vector2i] = []
+
+
+func set_interaction_cells(cells: Array[Vector2i]) -> void:
+	_interaction_cells.clear()
+	_interaction_cells.append_array(cells)
+
+
+func get_interaction_cells() -> Array[Vector2i]:
+	var result: Array[Vector2i] = []
+	result.append_array(_interaction_cells)
+	return result
+
+
+func get_output_item_type() -> StringName:
+	return &""
+
+
+func is_infinite() -> bool:
+	return false
+
+
+func take_item() -> ItemTransferResult:
+	return ItemTransferResult.rejected(ItemTransferResult.Status.INVALID_TARGET)
+
+
+func reset() -> void:
+	pass
