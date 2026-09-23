c67b1220d7201cfda9cf70cb990396b6507c0f0b
Ri0n72Y <tzy673570751@gmail.com>
2026-09-23 22:13:03 +0800

    [codex]fix(assembly): add rotate capability

--- scripts/assembly/assembly_capabilities.gd
+++ scripts/assembly/assembly_capabilities.gd
@@ -2,4 +2,5 @@ class_name AssemblyCapabilities
 extends RefCounted
 
 const CAN_MOVE: StringName = &"can_move"
+const CAN_ROTATE: StringName = &"can_rotate"
 const GRAB_DROP: StringName = &"grab_drop"
